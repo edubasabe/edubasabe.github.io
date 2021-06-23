b0b1a3571fdf4dd72f17912768003d6fd8d1e018
David <david.basabe@cencosud.cl>
2021-06-22 21:09:36 -0400

    :construction: About in progress

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -1,24 +1,9 @@
-const SKILLS = [
-  {
-    id: 1,
-    name:'HTML & CSS'
-  },
-  {
-    id: 2,
-    name: 'Javascript'
-  },
-  {
-    id: 3,
-    name: 'Typescript'
-  },
-];
-
-const ALL_SKILLS = {
-    languages: ['HTML & CSS', 'Javascript', 'Typescript'],
-    frontend: ['Vue','React','Angular', 'Gatsby', 'Next.js'],
-    backend: ['Node.js'],
-    testing: ['Cypress', 'Jest'],
-    others: ['Figma', 'Sketch'],
+const SKILLS = {
+  Languages: ['HTML & CSS', 'Javascript', 'Typescript'],
+  Frontend: ['Vue','React','Angular', 'Gatsby', 'Next.js'],
+  Backend: ['Node.js'],
+  Testing: ['Cypress', 'Jest'],
+  Others: ['Figma', 'Sketch'],
 };
 
 export default SKILLS;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import { Container, Heading, Flex, Box } from '@chakra-ui/layout'
 import { Text, Link, HStack, VStack, Icon } from '@chakra-ui/react'
 import { CheckIcon } from '@chakra-ui/icons';
-import React from 'react'
+import React, { Fragment } from 'react'
 import Layout from '../components/Layout/Layout'
 import SKILLS from '../constants/skills';
 
 const Works = () => {
+  const categories = Object.keys(SKILLS);
   return (
     <Layout>
       <Container maxW="container.xl" pt="4">
@@ -19,21 +20,34 @@ const Works = () => {
               color="blue.400">About</Heading>
             <Heading as="h3" mb="2">Hi! 👋 I'm David Basabe</Heading>
             <Heading as="h4" mb="2" size="md">A self taught <Link color="blue.400" href="https://en.wikipedia.org/wiki/Front-end_web_development" isExternal>Front End Developer</Link> from Venezuela 🇻🇪</Heading>
-            <Text color="gray.600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolorem praesentium assumenda dignissimos tempora nesciunt aut omnis sequi dicta debitis quasi, rerum facere est obcaecati. Dolorum et assumenda eum asperiores?</Text>
-
-            <Box>
-              <Heading as="h3">Skills</Heading>
-              <Heading as="h4" size="md">Languages</Heading>
-              {SKILLS.map((skill) => (
-                <HStack key={skill.id} align={'top'}>
-                  <Box color={'green.400'} px={2}>
-                    <Icon as={CheckIcon} />
-                  </Box>
+            <Text color="gray.600">
+              Web Developer with experience in application development and websites, working with agile methodologies, applying extensive knowledge in HTML, CSS, JavaScript, Vue, React, Angular and Node. MEVN Stack. <br /> <br />
+            </Text>
+            <Text color="gray.600" mb="8">
+              Notions of UI and UX design. Using design and prototype tools such as Sketch, Figma, Invision. With the ability to implement solutions thinking about the maintainability and extensibility of the code, team work, ethical and professional.
+            </Text>
+            <Box backgroundColor="white" py="8" px="6" rounded="xl" boxShadow="md">
+              <Heading as="h3" size="lg">Skills</Heading>
+              <Text mb="4" color="gray.600">These are the things I know about or use on a daily basis.</Text>
+              <HStack align={'top'} justify="space-between">
+              {categories.map(category => (
+                <Fragment key={category}>
                   <VStack align={'start'}>
-                    <Text fontWeight={600}>{skill.name}</Text>
+                  <Heading as="h4" size="md">{category}</Heading>
+                    {
+                      SKILLS[category].map((skill, idx) => (
+                      <HStack key={idx}>
+                        <Box color={'green.400'} px={2}>
+                          <Icon as={CheckIcon} />
+                        </Box>
+                        <Text fontWeight={600}>{skill}</Text>
+                      </HStack>
+                      ))
+                    }
                   </VStack>
-                </HStack>)
-              )}
+                </Fragment>
+              ))}
+              </HStack>
             </Box>
           </Box>
           <Box>Image</Box>
