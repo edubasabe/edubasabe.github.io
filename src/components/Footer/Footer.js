9e80dd3edaa8bf910d3e69769d07ff644f646fda
David <david.basabe@cencosud.cl>
2021-08-02 10:35:34 -0400

    :lipstick: Footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Box,
   chakra,
@@ -7,30 +7,27 @@ import {
   Text,
   useColorModeValue,
   VisuallyHidden,
-} from '@chakra-ui/react';
-import { FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+} from "@chakra-ui/react";
+import { FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-}) => {
+const SocialButton = ({ children, label, href }) => {
   return (
     <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
       w={8}
       h={8}
-      cursor={'pointer'}
-      as={'a'}
+      cursor={"pointer"}
+      as={"a"}
       href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
       _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
       <VisuallyHidden>{label}</VisuallyHidden>
       {children}
     </chakra.button>
@@ -40,25 +37,36 @@ const SocialButton = ({
 export default function SmallWithSocial() {
   return (
     <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
       <Container
         as={Stack}
-        maxW={'6xl'}
+        maxW={"6xl"}
         py={4}
-        direction={{ base: 'column', md: 'row' }}
+        direction={{ base: "column", md: "row" }}
         spacing={4}
-        justify={{ base: 'center', md: 'space-between' }}
-        align={{ base: 'center', md: 'center' }}>
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
         <Text>© 2021 Ebasabe. All rights reserved</Text>
-        <Stack direction={'row'} spacing={6}>
-          <SocialButton label={'Twitter'} href={'#'}>
+        <Stack direction={"row"} spacing={6}>
+          <SocialButton
+            label={"Twitter"}
+            href={"https://twitter.com/edubasabe"}
+          >
             <FaTwitter />
           </SocialButton>
-          <SocialButton label={'LinkedIn'} href={'https://www.linkedin.com/in/edubasabe'}>
+          <SocialButton
+            label={"LinkedIn"}
+            href={"https://www.linkedin.com/in/edubasabe"}
+          >
             <FaLinkedinIn />
           </SocialButton>
-          <SocialButton label={'Instagram'} href={'https://www.instagram.com/ebasabe_design/'}>
+          <SocialButton
+            label={"Instagram"}
+            href={"https://www.instagram.com/ebasabe_design/"}
+          >
             <FaInstagram />
           </SocialButton>
         </Stack>
