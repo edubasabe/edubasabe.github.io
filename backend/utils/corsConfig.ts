45dfbe70f8858de239966a2ffb89086ea2f97adf
David Basabe <edubasabe1@gmail.com>
2022-07-17 15:37:19 -0400

    :sparkles: Improves cors

--- backend/utils/corsConfig.ts
+++ backend/utils/corsConfig.ts
@@ -4,15 +4,23 @@ dotenv.config();
 
 const { DOMAINS_ALLOWED } = process.env;
 const whitelist = DOMAINS_ALLOWED?.split(",");
+const isProd = process.env.NODE_ENV === "production";
 
-const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    if (origin && whitelist?.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
+function setup(): CorsOptions {
+  if (isProd) {
+    return {
+      origin: function (origin, callback) {
+        if (origin && whitelist?.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      }
     }
-  },
-};
+  }
+  return {}
+}
+
+const corsOptions: CorsOptions = setup();
 
 export default corsOptions;
