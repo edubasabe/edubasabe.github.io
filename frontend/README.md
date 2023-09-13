991a5f89b97987a9e1737cf1da23310b02d73843
David Basabe <edubasabe1@gmail.com>
2023-09-13 17:38:41 -0300

    :sparkles: Contact page desktop

--- frontend/README.md
+++
@@ -1,9 +0,0 @@
-# Hi! this is my personal website
-
-Anytime you clone the repo you must create an .env.development file:
-
-```sh
-CONTENTFUL_ACCESS_TOKEN=XXXXXX
-```
-
-and get the access token from [Contentful](https://app.contentful.com/)
--- frontend/src/constants/skills.js
+++
@@ -1,9 +0,0 @@
-const SKILLS = {
-  Languages: ['HTML & CSS', 'Javascript', 'Typescript'],
-  Frontend: ['Vue','React','Angular', 'Gatsby', 'Next.js'],
-  Backend: ['Node.js'],
-  Testing: ['Cypress', 'Jest'],
-  Others: ['Figma', 'Sketch'],
-};
-
-export default SKILLS;
