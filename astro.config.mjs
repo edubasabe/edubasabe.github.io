2404b047a4cae005d46d3d74e9b5fbd1bcddc05b
David Basabe <edubasabe1@gmail.com>
2023-11-27 15:20:35 -0300

    :rocket: Sets deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://edubasabe.github.io",
   integrations: [tailwind()],
   image: {
     domains: ["ctfassets.net"],
